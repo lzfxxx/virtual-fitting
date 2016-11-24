129f4ba041d8aaf546155bb4fac169558a72f7c8
lzfxxx <lzfxxx@gmail.com>
2016-11-24 22:15:54 +0000

    0.0.0.0 to localhost

--- src/services/Global.js
+++ src/services/Global.js
@@ -2,7 +2,8 @@ var Global = {
   mheight: 170,
   mchest: 91,
   mwaist: 71,
-  url: 'http://0.0.0.0:5000/',
+  //url: 'http://0.0.0.0:5000/',
+  url: 'http://localhost:5000/',
   img: 'http://127.0.0.1:8000/files/'
 };
 
